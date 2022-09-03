93f629af6c64d4be64b768aab063783d451c758f
Thanakorn Wongklad <thanakorn.wong@kmutt.ac.th>
2022-09-03 16:24:28 +0700

    first commit

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,33 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  let min, med, max;
+  if (n1 < n2 && n1 < n3 && n2 < n3) {
+    min = n1
+    med = n2
+    max = n3
+  } else if (n1 < n2 && n1 < n3 && n3 < n2) {
+    min = n1
+    med = n3
+    max = n2
+  } else if (n2 < n1 && n2 < n3 && n1 < n3) {
+    min = n2
+    med = n1
+    max = n3
+  } else if (n2 < n1 && n2 < n3 && n3 < n1) {
+    min = n2
+    med = n3
+    max = n1
+  } else if (n3 < n1 && n3 < n2 && n1 < n2) {
+    min = n3
+    med = n1
+    max = n2
+  } else if (n3 < n1 && n3 < n2 && n2 < n1) {
+    min = n3
+    med = n2
+    max = n1
+  }
+  const obj = { min, med, max }
+  return obj
 }
 module.exports = minMedMax
