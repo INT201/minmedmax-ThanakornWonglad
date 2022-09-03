9bbcd37017599d9a915e67381dd59af9449a56c5
Thanakorn Wongklad <thanakorn.wong@kmutt.ac.th>
2022-09-03 16:36:44 +0700

    second Commit

--- minMedMax.js
+++ minMedMax.js
@@ -1,33 +1,33 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  let min, med, max;
-  if (n1 < n2 && n1 < n3 && n2 < n3) {
+  let min, mid, max;
+  if (n1 <= n2 && n1 <= n3 && n2 <= n3) {
     min = n1
-    med = n2
+    mid = n2
     max = n3
-  } else if (n1 < n2 && n1 < n3 && n3 < n2) {
+  } else if (n1 <= n2 && n1 <= n3 && n3 <= n2) {
     min = n1
-    med = n3
+    mid = n3
     max = n2
-  } else if (n2 < n1 && n2 < n3 && n1 < n3) {
+  } else if (n2 <= n1 && n2 <= n3 && n1 <= n3) {
     min = n2
-    med = n1
+    mid = n1
     max = n3
-  } else if (n2 < n1 && n2 < n3 && n3 < n1) {
+  } else if (n2 <= n1 && n2 <= n3 && n3 <= n1) {
     min = n2
-    med = n3
+    mid = n3
     max = n1
-  } else if (n3 < n1 && n3 < n2 && n1 < n2) {
+  } else if (n3 <= n1 && n3 <= n2 && n1 <= n2) {
     min = n3
-    med = n1
+    mid = n1
     max = n2
-  } else if (n3 < n1 && n3 < n2 && n2 < n1) {
+  } else if (n3 <= n1 && n3 <= n2 && n2 <= n1) {
     min = n3
-    med = n2
+    mid = n2
     max = n1
   }
-  const obj = { min, med, max }
+  const obj = { min, mid, max }
   return obj
 }
 module.exports = minMedMax
